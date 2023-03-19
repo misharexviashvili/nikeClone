9860488748594b0b3cd1bf04159762ee28ccc64c
Misha <misharexviashvili@yahoo.com>
2023-03-19 16:23:10 +0400

    exported flatlist to individual component

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,28 @@
+import { StyleSheet, FlatList, View, Image } from "react-native";
+import products from "../data/products";
+const ProductsScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      numColumns={2}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      )}
+    />
+  );
+};
+
+export default ProductsScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+});
