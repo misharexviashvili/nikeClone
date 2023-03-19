137e8464a3307cf556f00280e776b86530016453
Misha <misharexviashvili@yahoo.com>
2023-03-20 00:28:17 +0400

    totals added

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -1,16 +1,74 @@
-import { FlatList } from "react-native";
+import { FlatList, Pressable } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import cart from "../data/cart";
 import CartListItem from "../components/CartListItem";
+import { Fragment } from "react";
 const ShoppingCart = () => {
+  const shoppingCartTotal = () => (
+    <View style={styles.totalsContainer}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Subtotal</Text>
+        <Text style={styles.text}>410.00 US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Delivery</Text>
+        <Text style={styles.text}>10.00 US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.textBold}>Total</Text>
+        <Text style={styles.textBold}>420.00 US$</Text>
+      </View>
+    </View>
+  );
   return (
-    <FlatList
-      data={cart}
-      renderItem={({ item }) => <CartListItem cartItem={item} />}
-    />
+    <Fragment>
+      <FlatList
+        data={cart}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={shoppingCartTotal}
+      />
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Checkout</Text>
+      </Pressable>
+    </Fragment>
   );
 };
 
 export default ShoppingCart;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  totalsContainer: {
+    margin: 20,
+    paddingTop: 10,
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: "gray",
+  },
+  textBold: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 100,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
